bce585fa4c28bd4d95381e9e6311a2381ab7a70a
rahul <rahul52us@gmail.com>
2023-12-30 11:34:32 +0530

    merge rahul_frontend in master

--- src/pages/Dashboard/Trip/TripManagement/component/forms/EditTripForm.tsx
+++ src/pages/Dashboard/Trip/TripManagement/component/forms/EditTripForm.tsx
@@ -1,9 +1,10 @@
-import { observer } from "mobx-react-lite"
+import React from 'react'
 
-const EditTripForm = observer(() => {
+const EditTripForm = () => {
+  console.log(React)
   return (
     <div>EditTripForm</div>
   )
-})
+}
 
 export default EditTripForm
